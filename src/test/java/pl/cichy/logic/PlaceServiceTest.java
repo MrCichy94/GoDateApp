d97cad9ad52e6be7e8bb2f9c391a95e49148bc80
MrCichy94 <przemyslav.cichy@gmail.com>
2021-01-21 13:09:24 +0100

    C4_2021_PlaceService_Test_done

--- src/test/java/pl/cichy/logic/PlaceServiceTest.java
+++ src/test/java/pl/cichy/logic/PlaceServiceTest.java
@@ -1,5 +1,6 @@
 package pl.cichy.logic;
 
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pl.cichy.model.Comment;
@@ -11,6 +12,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
@@ -24,11 +26,10 @@ class PlaceServiceTest {
 
         //given
         var mockPlaceRepository = mock(PlaceRepository.class);
-        when(mockPlaceRepository.existsById(anyInt())).thenReturn(false);
+        when(mockPlaceRepository.existsById(1)).thenReturn(false);
 
         //and
         var mockCommentRepository = mock(CommentRepository.class);
-        when(mockCommentRepository.existsById(anyInt())).thenReturn(true);
 
         //and
         var mockComment = mock(Comment.class);
@@ -36,12 +37,12 @@ class PlaceServiceTest {
         //system under test
         var toTest = new PlaceService(mockCommentRepository, mockPlaceRepository);
 
-        try {
-            //when
-            toTest.createCommentForPlaceById(5, mockComment);
-        } catch (NoSuchElementException e) {
-            //then
-            assertThat(e).isInstanceOf(NoSuchElementException.class).hasMessageContaining("No place found");
-        }
+        //when
+        var exception = catchThrowable(()->toTest.createCommentForPlaceById(1, mockComment));
+
+        //than
+        assertThat(exception)
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessageContaining("No place found");
     }
 }
