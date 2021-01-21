0c9f1a13aa7389a22a66257fc5541fdb27b7d535
MrCichy94 <przemyslav.cichy@gmail.com>
2021-01-21 18:52:03 +0100

    C6_2021_Add_CorsCongigurer_RefactorMethod

--- src/main/java/pl/cichy/configuration/MvcConfiguration.java
+++ src/main/java/pl/cichy/configuration/MvcConfiguration.java
@@ -1,12 +1,9 @@
 package pl.cichy.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Set;
 
@@ -24,14 +21,4 @@ public class MvcConfiguration implements WebMvcConfigurer {
         interceptors.stream().forEach(registry::addInterceptor);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-            }
-        };
-    }
 }
